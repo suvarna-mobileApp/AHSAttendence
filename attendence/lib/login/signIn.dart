a48b7ccc7d221192a826a4a189263914f7ac4371
Suvarna Varadaraju <suvarna.varadaraju@almufeed.ae>
2024-02-02 09:09:41 +0400

    updated

--- attendence/lib/login/signIn.dart
+++ attendence/lib/login/signIn.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Colours.dart';
+
 class signIn extends StatelessWidget {
   const signIn({super.key});
 
@@ -20,6 +22,7 @@ class signInExample extends StatefulWidget {
 }
 
 class _signInExampleExampleState extends State<signInExample> with TickerProviderStateMixin {
+  final TextEditingController nameController = TextEditingController();
 
   @override
   void initState() {
@@ -37,18 +40,139 @@ class _signInExampleExampleState extends State<signInExample> with TickerProvide
       body: Center(
         child: Stack(
           children: <Widget>[
-            SizedBox.expand(
+            Container(
+              width: double.infinity,
+              height: double.infinity,
               child: Image.asset(
                 'assets/image/bg.jpg',
-                width: double.infinity,
-                height: double.infinity,
                 fit: BoxFit.cover,
+                height: double.infinity,
+                width: double.infinity,
+                alignment: Alignment.center,
               ),
             ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  alignment: Alignment.centerLeft,
+                  child: new Text("Employee Id/Email Id",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: _buildTextFieldWIthoutBorder(
+                    controller: nameController,
+                  ),
+                ),
+                Container(
+                    child: _buttons('Login', context)
+                ),
+              ],
+            ),
+           /* Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                //color: ColorConstants.transparent,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    alignment: Alignment.centerLeft,
+                    child: new Text("Employee Id/Email Id",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: _buildTextFieldWIthoutBorder(
+                      controller: nameController,
+                    ),
+                  ),
+                  Container(
+                      child: _buttons('Login', context)
+                  ),
+                ],
+              ),
+            ),*/
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildTextFieldWIthoutBorder({
+    required TextEditingController controller,
+    TextInputType? inputType,
+    int? maxLines,
+  }) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: TextField(
+        controller: controller,
+        keyboardType: inputType,
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: ColorConstants.kPrimaryColor,width: 0.5),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: ColorConstants.kPrimaryColor,width: 0.5),
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buttons(name, BuildContext context) {
+    return Center(
+        child: ButtonBar(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ButtonTheme(
+                minWidth: 200,
+                child: ElevatedButton(
+                  child: new Text(name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    elevation: 4,
+                    primary: ColorConstants.kPrimaryColor,
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                  ),
+                  onPressed: () {
+
+                  },
+                )),
+          ],
+        ));
   }
 
   double getTextSize(BuildContext context) {
